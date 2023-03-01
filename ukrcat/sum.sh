9a2df174a89f615450e4ebc535ce146233db1805
5HT <maxim@synrc.com>
2023-03-01 17:07:48 +0200

    ФАУ

---
+++ ukrcat/sum.sh
@@ -0,0 +1,7 @@
+file=ukrpost-2005.txt
+cat $file | grep Аркуш | wc -l
+cat $file | grep Зчіпка | wc -l
+cat $file | grep Блок | wc -l
+cat $file | grep Марка | wc -l
+cat $file | grep Стандарт | wc -l
+cat $file | grep "\·" | wc -l
